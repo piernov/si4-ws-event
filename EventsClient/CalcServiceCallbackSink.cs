1541e3df2b7938d8141843f792063fbb153669dc
piernov <piernov@piernov.org>
2018-03-29 08:52:02 +0200

    Finishied

---
+++ EventsClient/CalcServiceCallbackSink.cs
@@ -0,0 +1,22 @@
+﻿using EventsClient.CalcServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsClient
+{
+    class CalcServiceCallbackSink : ICalcServiceCallback
+    {
+        public void Calculated(int nOp, double dblNum1, double dblNum2, double dblResult)
+        {
+            Console.WriteLine("nOp: " + nOp + " dblNum1: " + dblNum1 + " dblNum2: " + dblNum2 + " dblResult: " + dblResult);
+        }
+
+        public void CalculationFinished()
+        {
+            Console.WriteLine("CalculationFinished");
+        }
+    }
+}
