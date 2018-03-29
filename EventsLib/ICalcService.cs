35412c6e3776707597ab2b06f6f1901573e6fa67
piernov <piernov@piernov.org>
2018-03-29 08:10:12 +0200

    Service hosted

---
+++ EventsLib/ICalcService.cs
@@ -0,0 +1,19 @@
+﻿//  Listing of ICalcService.cs
+using System;
+using System.Text;
+using System.ServiceModel;
+namespace EventsLib
+{
+    [ServiceContract(CallbackContract = typeof(ICalcServiceEvents))]
+    public interface ICalcService
+    {
+        [OperationContract]
+        void Calculate(int nOp, double dblNum1, double dblNum2);
+
+        [OperationContract]
+        void SubscribeCalculatedEvent();
+
+        [OperationContract]
+        void SubscribeCalculationFinishedEvent();
+    }
+}
